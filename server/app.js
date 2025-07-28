961bdf6e7c33dc5ad7ca9e9c47abf4ccf33a3d33
jms0802 <jms010802@naver.com>
2025-07-29 00:09:30 +0900

    fix: CORS Origin 수정

--- server/app.js
+++ server/app.js
@@ -23,8 +23,16 @@ const allowedOrigins = [
 
 
 app.use(cors({
-  origin: allowedOrigins,
-  credentials: true, // 쿠키 포함 허용
+  origin: function(origin, callback) {
+    // allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 
